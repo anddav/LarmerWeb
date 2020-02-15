e464210fa3369b48af108fb4b460940b279f805d
anddav <anders@kandav.com>
2020-02-15 23:11:27 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LarmerMain msg="Larmer" />
+      <!-- <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LarmerMain from './components/LarmerMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    LarmerMain
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/LarmerMain.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="larmer-main" :class="modifiers">
+    <h1 class="header">Larmer Web</h1>
+    <div class="row ">
+      <div class="col-lg row-content"><Button class="btn btn-lg btn-danger" @click="() => onClick('red')">Red</Button></div>
+      <div class="col-lg row-content"><Button class="btn btn-lg btn-success" @click="() => onClick('green')">Green</button></div>
+      <div class="col-lg row-content"><Button class="btn btn-lg btn-primary" @click="() => onClick('blue')">Blue</button></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LarmerMain",
+  data() {
+    return {
+      currentColor: String
+    } 
+  },
+  props: { 
+    msg: String 
+  },
+  computed: {
+    modifiers () {
+      return {
+        'larmer-bg-red': this.currentColor === 'red',
+        'larmer-bg-green': this.currentColor === 'green',
+        'larmer-bg-blue': this.currentColor === 'blue'
+      }
+    }
+  },
+  methods: {
+    onClick(color){
+      this.currentColor = color;
+    }
+  }
+}
+</script>
+<style>
+.larmer-main {
+  /* background: green; */
+  padding: 2rem;
+  min-height: 100vh;
+  text-align: center;
+}
+.larmer-bg-red {
+  background: red;
+}
+.larmer-bg-green {
+  background: green;
+}
+.larmer-bg-blue {
+  background: blue
+}
+.header {
+  color: darkslategray;
+}
+.row-content {
+  padding-top: 1rem;
+}
+</style>
